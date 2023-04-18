4d64077d6b06bd1aae7711d3d600749eb0c6693f
Mahmood-Darwish <mahmood2000334@gmail.com>
2023-04-18 16:07:52 +0300

    Connect the backend with the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
+import { useState } from "react"
+
 function App() {
-    const LIST_OF_QUESTIONS = [
+    const LIST_OF_QUESTIONS1 = [
         "1. Am the life of the party:",
         "2. Feel little concern for others:",
         "3. Am always prepared:",
@@ -25,6 +27,8 @@ function App() {
         "23. Get chores done right away:",
         "24. Am easily disturbed:",
         "25. Have excellent ideas:",
+    ]
+    const LIST_OF_QUESTIONS2 = [
         "26. Have little to say:",
         "27. Have a soft heart:",
         "28. Often forget to put things back in their proper place:",
@@ -51,13 +55,22 @@ function App() {
         "49. Often feel blue:",
         "50. Am full of ideas:",
     ]
+    const [recommendations, setRecommendations] = useState([])
+
     function handleSubmit(event) {
         event.preventDefault()
-        let res = []
+        const res = []
         for (let i = 0; i < event.target.length - 1; i++) {
-            res.push(event.target[i].value)
+            res.push(parseInt(event.target[i].value))
         }
-        console.log(res)
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ Answers: res }),
+        }
+        fetch("/recommend", requestOptions)
+            .then((response) => response.json())
+            .then((data) => setRecommendations(data))
     }
 
     return (
@@ -66,26 +79,84 @@ function App() {
                 <b>Instructions</b> In the form below, for each statement 1-50
                 mark how much you agree with on the scale 0-4, where 0=disagree,
                 1=slightly disagree, 2=neutral, 3=slightly agree and 4=agree, in
-                the box to the below it.
+                the box next to it. You will get an ordered list of
+                recommendations at the bottom of the page.
             </p>
             <form onSubmit={handleSubmit}>
-                <p>I...</p>
-                {LIST_OF_QUESTIONS.map((question, index) => {
-                    return (
-                        <div>
-                            <label>{question}</label>
-                            <select id={index}>
-                                <option value="0">0</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                            </select>
-                        </div>
-                    )
-                })}
-                <input type="submit"></input>
+                <p>
+                    <b>I...</b>
+                </p>
+                <div style={{ overflow: "auto" }}>
+                    <div style={{ float: "left", paddingLeft: "100px" }}>
+                        {LIST_OF_QUESTIONS1.map((question, index) => {
+                            return (
+                                <div style={{ padding: "5px" }}>
+                                    <label style={{ paddingRight: "2px" }}>
+                                        {question}
+                                    </label>
+                                    <select id={index}>
+                                        <option value="0">0</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                    </select>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div style={{ float: "right", paddingRight: "100px" }}>
+                        {LIST_OF_QUESTIONS2.map((question, index) => {
+                            return (
+                                <div style={{ padding: "5px" }}>
+                                    <label style={{ paddingRight: "2px" }}>
+                                        {question}
+                                    </label>
+                                    <select id={index}>
+                                        <option value="0">0</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                    </select>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+                <p>
+                    <b>Room related questions: </b>
+                </p>
+                <div style={{ padding: "5px" }}>
+                    <label style={{ paddingRight: "2px" }}>
+                        What type of room do you want?
+                    </label>
+                    <select id>
+                        <option value="0">2 People Room</option>
+                        <option value="1">5 People Room</option>
+                    </select>
+                </div>
+                <div style={{ padding: "5px" }}>
+                    <label style={{ paddingRight: "2px" }}>
+                        How important is it for you to get the type of room that
+                        you want?
+                    </label>
+                    <select>
+                        <option value="0">Low Importance</option>
+                        <option value="1">Medium Importance</option>
+                        <option value="2">High Importance</option>
+                    </select>
+                </div>
+                <input type="submit" style={{ padding: "5px" }}></input>
             </form>
+            <p>
+                <b>List of Recommendations: </b>
+            </p>
+            <div>
+                {recommendations.map((room) => {
+                    return <div style={{ padding: "5px" }}>Room {room}</div>
+                })}
+            </div>
         </div>
     )
 }
